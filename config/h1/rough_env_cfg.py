53d80505b14f31e3e69f81d8fa102be8d4680f78
egeozgul <egeozgul@gmail.com>
2026-06-30 12:27:28 -0400

    Create rough_env_cfg.py

---
+++ config/h1/rough_env_cfg.py
@@ -0,0 +1,41 @@
+egeozgul
+egeozgul
+Update README.md
+bbf372e
+ · 
+now
+plots
+	
+Add training plots and plot script
+	
+3 weeks ago
+README.md
+	
+Update README.md
+	
+now
+h1_demo.gif
+	
+Initial commit: H1 humanoid locomotion with Isaac Lab
+	
+3 weeks ago
+h1_demo_multi.gif
+	
+Initial commit: H1 humanoid locomotion with Isaac Lab
+	
+3 weeks ago
+play_follow_cam.py
+	
+Add play script and keyboard controller
+	
+3 weeks ago
+plot_training.py
+	
+Add training plots and plot script
+	
+3 weeks ago
+robot_controller.py
+	
+Add play script and keyboard controller
+	
+3 weeks ago
